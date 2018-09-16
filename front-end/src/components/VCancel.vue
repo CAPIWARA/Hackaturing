0e2d55edd138ac9f61a53d3a4d9300ef59b63503
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-09-16 07:44:27 -0300

    :sparkles: creates check & cancel buttons

---
+++ front-end/src/components/VCancel.vue
@@ -0,0 +1,40 @@
+<template>
+  <button :class="[ 'v-cancel', '-' + size.toLowerCase() ]">
+    <img class="icon" src="@/assets/icons/Cancel.svg" alt="Rejeitar" />
+  </button>
+</template>
+
+<script>
+  export default {
+    props: {
+      size: {
+        type: String,
+        default: 'MD',
+        validator: (size) => [ 'SM', 'MD' ].includes(size),
+      },
+    },
+  };
+</script>
+
+<style lang="stylus">
+  .v-cancel
+    display: block
+    border: 2px solid #ff4757
+    padding: 5px
+    outline: none
+    border-radius: 50%
+    cursor: pointer
+    transition: background-color .5s
+
+    &.-sm
+      width: 30px
+      height: @width
+
+    &.-md
+      width: 50px
+      height: @width
+
+    &:active
+      background-color: rgba(#ff4757, .25)
+      transition: background-color .05s
+</style>
---
+++ front-end/src/components/VCheck.vue
@@ -0,0 +1,45 @@
+<template>
+  <button :class="[ 'v-check', '-' + size.toLowerCase() ]">
+    <img class="icon" src="@/assets/icons/Check.svg" alt="Confirmar" />
+  </button>
+</template>
+
+<script>
+  export default {
+    props: {
+      size: {
+        type: String,
+        default: 'MD',
+        validator: (size) => [ 'SM', 'MD' ].includes(size),
+      },
+    },
+  };
+</script>
+
+<style lang="stylus">
+  .v-check
+    display: block
+    border: 2px solid #2acf85
+    padding: 5px
+    outline: none
+    border-radius: 50%
+    cursor: pointer
+    transition: background-color .5s
+
+    &.-sm
+      width: 30px
+      height: @width
+
+    &.-md
+      width: 50px
+      height: @width
+
+    &:active
+      background-color: rgba(#2acf85, .25)
+      transition: background-color .05s
+
+    // &.-success
+    //   border-color: #2acf85
+    // &.-failure
+    //   border-color: #ff4757
+</style>
